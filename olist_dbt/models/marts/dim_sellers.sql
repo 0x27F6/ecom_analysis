c338f2bf7fdaf423235c8ccb0b447badd05d4fc3
0x27F6 <bad.shade.blue@gmail.com>
2026-04-21 13:32:53 -0400

    removed: redundant columns that exist in mart_seller_performance_single_orders

--- olist_dbt/models/marts/dim_sellers.sql
+++ olist_dbt/models/marts/dim_sellers.sql
@@ -8,15 +8,6 @@ select
     -- keys
     seller_id,
 
-    -- stats
-    total_items_sold,
-    unique_order_count,
-    total_revenue,
-    total_freight,
-    review_count,
-    avg_review_score,
-    avg_days_to_carrier,
-
     -- location
     seller_zip,
     seller_city,
